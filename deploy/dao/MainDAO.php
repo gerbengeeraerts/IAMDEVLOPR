bdee630ab4dea51110a0274aa38b8edb645cd840
Thibault Maekelbergh <thibault.maekelbergh@student.howest.be>
2015-03-17 19:58:55 +0100

    Included _functions.example.php
    This file has some example functions you can include in the DAO to
    perform CRUD tasks

--- deploy/dao/MainDAO.php
+++ deploy/dao/MainDAO.php
@@ -3,13 +3,8 @@ require_once WWW_ROOT. DS . 'dao'. DS . 'DAO.php';
 
 class MainDAO extends DAO {
 
-    // Example function
-    public function selectAll() {
-      $sql = "SELECT * FROM `table`";
-      $stmt = $this->pdo->prepare($sql);
-      $stmt->execute();
-      return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
+  // Take a look in the _functions.example.php
+  // to get some examples of DAO CRUD functions
 
 }
 
---
+++ deploy/dao/_functions.example.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * This file includes some common example functions
+ * for usage in the DAO's.
+ * Copy/paste examples into exisiting files, but dont
+ * include this file in your routers.
+ */
+
+ /**
+  * QUERYING - READ
+  */
+
+ public function selectAll() {
+   $sql = "SELECT * FROM `table`";
+   $stmt = $this->pdo->prepare($sql);
+   $stmt->execute();
+   return $stmt->fetchAll(PDO::FETCH_ASSOC);
+ }
+
+ public function selectById($id) {
+   $sql = "SELECT * FROM `table` WHERE `id` = :id";
+   $stmt = $this->pdo->prepare($sql);
+   $stmt->bindValue(':id', $id);
+   $stmt->execute();
+   return $stmt->fetch(PDO::FETCH_ASSOC);
+ }
+
+ /**
+  * STORE - CREATE
+  */
+
+  public function insert($data) {
+    $errors = $this->getValidationErrors($data);
+    if(empty($errors)){
+      $sql = "INSERT INTO `table` (`id`, `rowvalue`)
+                VALUES (:id, :rowvalue)";
+      $stmt = $this->pdo->prepare($sql);
+      $stmt->bindValue(':id', '');
+      $stmt->bindValue(':rowvalue', $data['value']);
+      if($stmt->execute()) {
+        $insertedId = $this->pdo->lastInsertId();
+        return $this->selectById($insertedId);
+      }
+    }
+    return false;
+  }
+
+ /**
+  * VALIDATION
+  */
+
+ public function getValidationErrors($data) {
+   $errors = array();
+   if(empty($data['value'])) {
+     $errors['value'] = "Error Message";
+   }
+   return $errors;
+ }
+
+?>
